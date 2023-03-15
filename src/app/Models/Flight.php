0408bc3e260cfee467cdb6bbef58de2c2e660294
sebashack <sebastian0092@gmail.com>
2023-03-15 06:33:23 -0500

    Implement model

---
+++ src/app/Models/Flight.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    protected $fillable = ['type', 'price'];
+
+    public function getType(): string
+    {
+        return $this->attributes['type'];
+    }
+
+    public function setType(string $type): void
+    {
+        $this->attributes['type'] = $type;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->attributes['price'] = $price;
+    }
+}
